dbcf3431630196247537dfe03f33fdb47987107d
Sarthak0967 <sarthakdoc09@gmail.com>
2025-10-17 19:52:55 +0530

    fix: update avatar image source in UseDropdown component

--- components/UseDropdown.tsx
+++ components/UseDropdown.tsx
@@ -44,7 +44,7 @@ const UseDropdown = ({ user, initialStocks }: {user: User, initialStocks: StockW
                 <DropdownMenuLabel>
                     <div className="flex relative items-center gap-3 py-2">
                         <Avatar className="h-10 w-10">
-                            <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
+                            <AvatarImage src="https://www.flaticon.com/free-icons/people" title="people icons" />
                             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                                 {user.name[0]}
                             </AvatarFallback>
